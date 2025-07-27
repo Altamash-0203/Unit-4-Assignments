1bd231cdedd96b5f3c40e059447a0683dd61ee67
Altamash Shaikh <mr.shaikh.altamash0203@gmail.com>
2025-07-27 17:54:45 +0530

    List Rnder In React

---
+++ src/Introduction-to-React-with_JSX&Babel/Q12.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function RenderList(){
+
+    return(
+        <div>
+            <ul>
+                <li>React</li>
+                <li>Javascript</li>
+                <li>CSS</li>
+            </ul>
+        </div>
+    )
+}
+
+export default RenderList
